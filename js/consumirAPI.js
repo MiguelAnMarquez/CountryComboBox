8f61657ea066f4f50e36bc4f018e0f5dba3921ea
MiguelAnMarquez <125562185+MiguelAnMarquez@users.noreply.github.com>
2023-05-04 10:06:01 -0500

    Update consumirAPI.js

--- js/consumirAPI.js
+++ js/consumirAPI.js
@@ -79,7 +79,7 @@ function showCountryData() {
     Row1.appendChild(td)
 
     td = document.createElement('td');
-    hText = document.createTextNode(countries[select.value].capital[1]);
+    hText = document.createTextNode(Object.values(countries[select.value].capital)[0]);
     td.appendChild(hText)
     Row1.appendChild(td)
 
